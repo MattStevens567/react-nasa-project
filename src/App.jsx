5d53eaadd0fdeab3e118977c11d4f4087072f8cf
MattStevens567 <mattstevens567@gmail.com>
2024-09-19 11:31:55 +1200

    Finished Project
    - Bulk of project finished, uploading this so I can run it through Netlify as a hosting service

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,89 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import SideBar from './components/SideBar';
+import Main from './components/Main';
+import Footer from './components/Footer';
+import { useState, useEffect } from 'react';
+
+const NASA_KEY = import.meta.env.VITE_NASA_API_KEY;
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [showModal, setShowModal] = useState(false);
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(false);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    function handleToggleModal() {
+        setShowModal(!showModal);
+    }
+
+    useEffect(() => {
+        async function fetchAPIData() {
+            const url = 'https://api.nasa.gov/planetary/apod' + `?api_key=${NASA_KEY}`;
+
+            const todayString = new Date().toDateString();
+
+            const localKey = `NASA-${todayString}`;
+            if (localStorage.getItem(localKey)) {
+                const apiData = JSON.parse(localStorage.getItem(localKey));
+                const imageDate = apiData?.date;
+                const today = getUTCCurrentDate();
+                if (isAPIDataOld(imageDate, today)) {
+                    setData(apiData);
+                    console.log('Fetched from cache today');
+                    return; // So we don't pull data unnecessarily
+                }
+                console.log('APIData is old! Requesting new Data');
+            }
+            localStorage.clear();
+
+            try {
+                const res = await fetch(url);
+                const apiData = await res.json();
+                localStorage.setItem(localKey, JSON.stringify(apiData));
+                setData(apiData);
+                console.log('Fetched from API today');
+            } catch (err) {
+                console.log(err.message);
+            }
+        }
+        fetchAPIData();
+    }, []);
+
+    return (
+        <>
+            {data ? (
+                <Main data={data} />
+            ) : (
+                <div className="loadingState">
+                    <i className="fa-solid fa-gear"></i>
+                </div>
+            )}
+            {data && showModal && <SideBar data={data} handleToggleModal={handleToggleModal} />}
+
+            {data && <Footer data={data} handleToggleModal={handleToggleModal} />}
+        </>
+    );
 }
 
-export default App
+function getUTCCurrentDate() {
+    const date = new Date();
+    const year = date.getUTCFullYear();
+    const month = String(date.getUTCMonth() + 1).padStart(2, '0'); // Months are zero-based, so we add 1
+    const day = String(date.getUTCDate()).padStart(2, '0');
+
+    return `${year}-${month}-${day}`;
+}
+
+function isAPIDataOld(apiDate, today) {
+    const parsedAPIDate = new Date(apiDate);
+    const parsedToday = new Date(today);
+
+    if (parsedAPIDate < parsedToday) {
+        console.log(parsedToday);
+        console.log(parsedAPIDate);
+        return false;
+    } else {
+        console.log('API Data is still valid');
+        return true;
+    }
+}
+
+export default App;
